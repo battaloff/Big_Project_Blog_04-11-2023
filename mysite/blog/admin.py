509bc4ef6cf31b4bb23da099fbb0f968535ec162
battaloff <battaloff@mail.com>
2023-11-04 22:49:37 +0500

    Продолжение 04-11-2023 22:49

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Posts
 
-# Register your models here.
+
+@admin.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    autocomplete_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
