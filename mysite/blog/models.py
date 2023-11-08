3bac76d87455b880783b397211ffc7e924453d8f
Ravshan <battaloff@mail.ru>
2023-11-08 10:47:48 +0500

    На работе

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset() \
+            .filter(status=Posts.Status.PUBLISHED)
+
+
 class Posts(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -19,6 +25,8 @@ class Posts(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ['-publish']
