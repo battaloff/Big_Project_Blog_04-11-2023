051922d239231ea66d07387be863f88a7431dd61
Ravshan <battaloff@mail.ru>
2023-11-18 17:14:08 +0500

    18-11-2023 На работе пытаюсь сделать пуш в новую ветку master_WORK

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,21 @@
-from .models import Posts
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
+from .forms import EmailPostForm
+from .models import Posts
+
+from django.core.mail import send_mail
+from django.conf import settings
 
 
-def post_list(request):
-    posts = Posts.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Posts.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -20,3 +29,31 @@ def post_detail(request, year, month, day, post):
                   'blog/post/detail.html',
                   {'post': post})
 
+
+def post_share(request, post_id):
+    # Извлечь пост по его идентификатору id
+    post = get_object_or_404(Posts,
+                             id=post_id,
+                             status=Posts.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
